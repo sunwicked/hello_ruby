6aadd1739d8b2e545e0987a85cc99dd864bd74c5
sunny <sunnykrditya@gmail.com>
2020-04-27 23:46:55 +0530

    Final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -20,11 +22,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
 
     if @post.update(post_params)
       redirect_to @post
@@ -34,7 +34,6 @@ class PostsController < ApplicationController
   end
 
 def destroy 
-	@post =  Post.find(params[:id])
 	@post.destroy
 
 	redirect_to posts_path
@@ -42,12 +41,15 @@ end
 
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post  
+	@post = Post.find(params[:id])
   end
 end
